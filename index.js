16215426d83684c963229ce6061b98d684e95eaa
ExxonExxon <132812480+ExxonExxon@users.noreply.github.com>
2025-09-04 13:54:32 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -22,28 +22,5 @@ function showAlert(message, isRed) {
 
 // EMAIL
 
-(function() {
-    emailjs.init("iPXyFM_v59tYW7l2E"); // from EmailJS dashboard
-})()
-function sendEmail(e) {
-    e.preventDefault();
-    const btn = e.target.querySelector("button");
-    btn.disabled = true;
-    btn.classList.add("contact-me-button-disabled");
-    btn.textContent = "Sending...";
-
-    emailjs.sendForm("service_9b4h1am", "template_nb8qjqq", e.target)
-        .then(() => {
-            btn.disabled = false;
-            btn.classList.remove("contact-me-button-disabled");
-            btn.textContent = "Contact";
-            showAlert("Message sent!", false); // show green alert
-        }, (error) => {
-            btn.disabled = false;
-            btn.classList.remove("contact-me-button-disabled");
-            btn.textContent = "Contact";
-            showAlert("Failed: " + JSON.stringify(error), true); // show red alert
-        });
-}
 
 
